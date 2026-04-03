965b7a6bebf58914b1c037794861c7be9c6a6476
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2026-04-03 22:44:54 +0000

    Change church logo to my logo

--- components/Image.tsx
+++ components/Image.tsx
@@ -9,8 +9,8 @@ const ChurchImage = ({ className }: MyImageProps) => {
   return (
   <>
   <Image 
-  src="/images/icgc-pht.jpeg" 
-  alt="icgc-logo" 
+  src="/images/ad-logo.png" 
+  alt="ad-logo" 
   width={400} 
   height={400}
   priority />
